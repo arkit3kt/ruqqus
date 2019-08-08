1b9f6e9174ef6cb036a454e06a2c50e5d3cb70e8
arkitekt <exodust@protonmail.com>
2019-08-08 00:56:59 -0400

    resend activation email

--- ruqqus/routes/settings.py
+++ ruqqus/routes/settings.py
@@ -10,6 +10,11 @@ def settings_post(v):
 
     updated=False
 
+    if request.form.get("resend_activation"):
+        if not send_verification_email(v):
+            return render_template("settings.html", v=v, error="There was an issue sending the E-mail.")
+        return render_template("settings.html", v=v, msg="Your Activation E-Mail has been resent")
+
     if request.form.get("new_password"):
         if request.form.get("new_password") != request.form.get("cnf_password"):
             return render_template("settings.html", v=v, error="Passwords do not match.")
