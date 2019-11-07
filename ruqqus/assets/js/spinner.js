beb64750bb67a4c10d3eb57f8c61bb0d12ef116f
kek7198 <memezdank140@gmail.com>
2019-11-07 11:43:09 -0600

    Update spinner.js

--- ruqqus/assets/js/spinner.js
+++ ruqqus/assets/js/spinner.js
@@ -8,3 +8,25 @@ $(document).ready(function() {
       );
     });
 });
+
+$(document).ready(function() {
+	$('#register_form').submit(function() {
+      // disable button
+      $("#login_button").prop("disabled", true);
+      // add spinner to button
+      $("#login_button").html(
+        `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Registering`
+      );
+    });
+});
+
+$(document).ready(function() {
+	$('#submit_form').submit(function() {
+      // disable button
+      $("#submit_button").prop("disabled", true);
+      // add spinner to button
+      $("#submit_button").html(
+        `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Creating post`
+      );
+    });
+});
