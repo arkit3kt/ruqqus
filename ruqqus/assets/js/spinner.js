9c604f8e78a834ff13d293264228c52a7d874c0c
kek7198 <memezdank140@gmail.com>
2019-11-07 11:07:55 -0600

    login form on submit

--- ruqqus/assets/js/spinner.js
+++ ruqqus/assets/js/spinner.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	$("#login-form").on("submit", function(){
     $("#login_button").click(function() {
       // disable button
       $(this).prop("disabled", true);
@@ -7,4 +8,5 @@ $(document).ready(function() {
         `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Signing in`
       );
     });
+});
 });
