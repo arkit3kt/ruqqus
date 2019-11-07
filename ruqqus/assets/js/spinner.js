1de46464b3100ac4a768478147ab262542e662e9
kek7198 <memezdank140@gmail.com>
2019-11-07 10:59:06 -0600

    spinner js

---
+++ ruqqus/assets/js/spinner.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#login_button").click(function() {
+      // disable button
+      $(this).prop("disabled", true);
+      // add spinner to button
+      $(this).html(
+        `<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Signing in`
+      );
+    });
+});
