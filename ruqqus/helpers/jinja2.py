79fee4125bf9c88e53d60c18a74a1a18518a69e9
kek7198 <memezdank140@gmail.com>
2019-11-08 21:48:48 -0600

    Update jinja2.py

--- ruqqus/helpers/jinja2.py
+++ ruqqus/helpers/jinja2.py
@@ -6,8 +6,6 @@ from ruqqus.classes.user import User
 from ruqqus.classes.ips import IP
 from ruqqus.__main__ import app, db, cache
 
-youtube_regex=re.compile("^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*")
-
 @app.template_filter("users_here")
 def users_here(x):
 
@@ -21,16 +19,3 @@ def users_here(x):
 def total_users(x):
 
     return db.query(User).filter(text("ban_state>-1")).count()
-
-@app.template_filter("youtube_embed")
-def youtube_embed(url):
-
-    try:
-        yt_id=re.match(youtube_regex, url).group(2)
-    except AttributeError:
-        return "error"
-
-    if yt_id and len(yt_id)==11:
-        return yt_id
-    else:
-        return "error"
